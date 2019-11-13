484955b10e6d9706ae6795fce4b7ef1e9e0dbaef
ricardo <ricardo_lopes96@hotmail.com>
2019-11-13 00:36:38 +0000

    AuthServer: add a simple user signup with email verification (not tested yet)

--- Project/AuthServer/auth/auth/settings.py
+++ Project/AuthServer/auth/auth/settings.py
@@ -120,3 +120,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+EMAIL_USE_TLS = True
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'sirsmail1920@gmail.com'
+EMAIL_HOST_PASSWORD = 'sirsMail2019'
+EMAIL_PORT = 587
